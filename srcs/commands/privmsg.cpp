aa0d3597d1feefaf25df30e059ed439f1c5de50c
LuxoJr22 <mathisbodu83@gmail.com>
2024-04-05 14:34:04 +0200

    privmsg

--- srcs/commands/privmsg.cpp
+++ srcs/commands/privmsg.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   privmsg.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpelazza <mpelazza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luxojr <luxojr@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 12:43:37 by mboyer            #+#    #+#             */
-/*   Updated: 2024/04/05 09:47:13 by mpelazza         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:07:59 by luxojr           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,8 @@ void send_all_fd(Server *server, std::vector<std::string> split_msg, int i)
 		std::map<std::string, User *>::iterator it = server->_channels[target]->_users.begin();
 		while (it != server->_channels[target]->_users.end())
 		{
-			std::cout << it->second->_id << std::endl;
-			if (it->second->_id != i - 1)
-				send(server->_fds[it->second->_id + 1].fd, join_response.c_str(), join_response.length(), 0);
+			if (it->second->_id != i)
+				send(server->_fds[it->second->_id].fd, join_response.c_str(), join_response.length(), 0);
 			it ++;
 		}
 	}
