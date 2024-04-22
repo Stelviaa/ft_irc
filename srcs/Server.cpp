af8ce615861081264d2f963f04c1bf5c25778e47
olmayto <mayou1369ios@gmail.com>
2024-04-22 13:47:51 +0200

    Quit message ok

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: mpelazza <mpelazza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 10:11:22 by sforesti          #+#    #+#             */
-/*   Updated: 2024/04/22 12:04:12 by mpelazza         ###   ########.fr       */
+/*   Updated: 2024/04/22 13:47:08 by mpelazza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,7 +150,7 @@ void	Server::kickUser(int id, std::string msg) {
 		std::string chan = this->_users[id]->_channels[i];
 		std::map<std::string, User *>::iterator it = this->_channels[chan]->_users.begin();
 		while (it != this->_channels[chan]->_users.end()) {
-			if (it->second->_id != id)
+			if (it->second->_id - 1 != id)
 				send(this->_fds[it->second->_id].fd, msg.c_str(), msg.length(), 0);
 			it ++;
 		}
